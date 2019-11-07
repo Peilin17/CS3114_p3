d2403c482b5fac1f8055d77c42ece6e18436e650
rnzho <rnzho@DESKTOP-JU8RL3J>
2019-11-07 14:23:13 -0500

    1k2能跑了

--- src/ExternalSort.java
+++ src/ExternalSort.java
@@ -35,6 +35,7 @@ public class ExternalSort {
     private int size;
     private boolean finishReading;
     private int heapSize = 0;
+    private int heapSizeHelp = 0;
 
     public ExternalSort(String filename) throws FileNotFoundException {
         index = 0;
@@ -187,6 +188,7 @@ public class ExternalSort {
     private void clearHeap() throws IOException {
 //        FileOutputStream fi = new FileOutputStream(f);
 //        DataOutputStream fin = new DataOutputStream(fi);
+        heapSizeHelp = heapSize;
         int i = 0;
         for (; i < heapSize; i++) {
             Ascore t = extractMax();
@@ -213,6 +215,10 @@ public class ExternalSort {
      */
     private void mutiMerge() throws IOException {
         ArrayList<ArrayList<Ascore>> runs = new ArrayList<ArrayList<Ascore>>();
+        if(index == 0)
+        {
+            index++;
+        }
         pivot = new int[index];
         if (HEAP_SIZE / index < 1024) { // 这下面是run的数量超过8个，所以每一个run不能取完不然内存超8block
             // 取每一个run的前 （总8block容量/run的数量）
@@ -380,11 +386,8 @@ public class ExternalSort {
      */
     public Ascore extractMax() {
         Ascore popped = heap[1];
-        int length = heap.length - 1;
-        if (length >= 0) {
-            heap[1] = heap[length];
-            maxHeapify(1);
-        }
+        heap[1] = heap[--heapSizeHelp];
+        maxHeapify(1);
         return popped;
     }
 
